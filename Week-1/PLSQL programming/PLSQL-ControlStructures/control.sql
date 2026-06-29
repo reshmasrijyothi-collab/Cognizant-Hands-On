634c982d9e0c947985aa71a05cdb9e6b6c7ccf28
reshmasrijyothi-collab <reshmasrijyothi@gmail.com>
2026-06-29 19:24:19 +0530

    Add files via upload

---
+++ Week-1/PLSQL programming/PLSQL-ControlStructures/control.sql
@@ -0,0 +1,9 @@
+
+BEGIN
+ FOR c IN (SELECT customer_id,age FROM customers) LOOP
+  IF c.age>60 THEN
+   UPDATE loans SET interest_rate=interest_rate-1 WHERE customer_id=c.customer_id;
+  END IF;
+ END LOOP;
+END;
+/
