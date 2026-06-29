baa6f9db4566dcc445e7ef82f7563729371d5209
reshmasrijyothi-collab <reshmasrijyothi@gmail.com>
2026-06-29 19:26:51 +0530

    Add files via upload

---
+++ Week-1/TDD using JUnit5 and Mockito/JUnit-Assertions/AssertionsTest.java
@@ -0,0 +1,4 @@
+
+import static org.junit.jupiter.api.Assertions.*;import org.junit.jupiter.api.Test;
+public class AssertionsTest{
+@Test void test(){assertEquals(5,2+3);assertTrue(true);assertFalse(false);assertNull(null);assertNotNull(new Object());}}
